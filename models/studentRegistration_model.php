c8b520bd3570ae90536a1e235ff2b8bb5b48554e
S.Nitharshana <2018is073@stu.ucsc.cmb.ac.lk>
2020-10-23 19:46:47 +0530

    staff part

---
+++ models/studentRegistration_model.php
@@ -0,0 +1,80 @@
+<?php
+
+class studentRegistration_Model extends Model{
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function listHalls(){
+
+        return $this->db->listAll("hall");
+    
+
+    }
+
+    public function listSubjects(){
+
+        return $this->db->listAll("subject");
+        
+
+    }
+
+
+    public function getUser($id){
+
+        return $this->db->listWhere('user',array('nic','first_name','last_name','gender','email','contact_no','user_status','user_type'),"nic='$id'");
+    }
+
+    public function create($data){
+        $this->db->insert("user","(username,type)","('".$data['email']."','student')");
+
+        $userID = $this->db->listWhere("id","user","username='".$data['email']."'");
+        $num = mysqli_fetch_assoc($userID);
+
+        $this->db->insert('student',"(fname,mname,lname,tel_no,address,NIC,DOB,gender,email,school,grade,stream,subject1,subject2,subject3,user_id)","('".$data['fname']."','".$data['mname']."','".$data['lname']."',".$data['tel_no'].",'".$data['address']."','".$data['NIC']."','".$data['DOB']."','".$data['gender']."','".$data['email']."','".$data['school']."','".$data['grade']."','".$data['stream']."','".$data['subject1']."','".$data['subject2']."','".$data['subject3']."',".$num['id'].")");
+
+        $reg_no = $this->db->listWhere("stu_reg_no","student","NIC='".$data['NIC']."'");
+        $reg = mysqli_fetch_assoc($reg_no);
+
+        $this->db->insert('parent',"(name,tel_no,stu_reg_no)","('".$data['name']."','".$data['tel']."',".$reg['stu_reg_no'].")");
+
+        $this->db->insert('studentSubject',"(subject1,subject2,subject3,stu_reg_no)","('".$data['subject1']."','".$data['subject2']."','".$data['subject3']."',".$reg['stu_reg_no'].")");
+
+
+ 
+
+    }
+ 
+        
+    }
+
+    /*public function update($data){
+
+        $this->db->update('user',array(
+            'nic' => $data['nic'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'gender' => $data['gender'],
+            'email' => $data['email'],
+            'username' => $data['email'],
+            'contact_no' => $data['contact_no'],
+            'user_status' => $data['user_status'],
+            'user_type' => $data['user_type']),"nic = '{$data['nic']}'");
+
+    }
+
+    public function delete($id){
+        $data = $this->db->listWhere('user',array('user_type'),"nic='$id'");
+
+        if($data['user_type']=='owner'){
+            return false;
+        } else{
+            $this->db->delete('user',"nic='$id'");
+        }
+
+    }*/
+
+
+
+
---
+++ views/staff/staffSalaryDetails.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Staff_Salary_Details</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<script src="https://kit.fontawesome.com/b481b35adc.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="http://localhost/IMS_Vidarsha/public/css/staffNavigationBar">
+<link rel="stylesheet" href="http://localhost/IMS_Vidarsha/public/css/staffSalaryDetails">
+
+</head>
+<body>
+
+
+<!--------------------------------------Navigation bar-------------------------------------->
+<div class="row">
+  <div class="leftNav">
+  <img src="<?php echo URL; ?>public/img/logo.png" width = "50%" height = "100px" style= "margin-left: 25%">
+	<ul>
+	  <li><a href="staffDashboard"><i class="fas fa-home"></i>Dashboard</a></li>
+      <li><a href="studentRegistration"><i class="fa fa-user-o"></i>Register Student</a></li>
+      <li><a href="updateStudent"><i class="fas fa-user-edit"></i>View Student</a></li>
+      <li><a href="enrollStudent"><i class="fa fa-user-o"></i>Enroll Student</a></li>
+      <li><a href="markAttendance"><i class="fas fa-users"></i>Mark Attendance</a></li>
+      <li><a href="collectClassFees"><i class="fa fa-money"></i>Collect fees</a></li>
+      <li><a href="staffSalaryDetails"><i class="fas fa-money-bill-wave"></i>Salary Details</a></li>
+	</ul>
+	<div class="chip"><img src="<?php echo URL; ?>public/icons/Logout.png" alt="Person" width="96" height="96">Log out</div>
+	<div class="chip" ><img src="<?php echo URL; ?>public/icons/School Director_30px.png" alt="Person" width="96" height="96">Profile</div>
+  </div>
+  <div class="header">
+	  <h2 style="text-indent:10px;margin-top:8px;margin-left:18%;position:absolute;"><i class="fas fa-money-bill-wave"></i>Staff Salary Details</h2>
+	  <div class="chip"><img src="<?php echo URL; ?>public/icons/School Director_30px.png" alt="Person" width="96" height="96">Staff Name</div>
+  </div>
+
+  <!-----------------------------------Middle contant----------------------->
+  <div class="middle" style="background-color:white;">	
+	  <div class="wrapper">
+		  <div class="title">
+			  <h3>Salary payment details</h3>
+		  </div>
+	  <div class="details">
+      <p>Payment date :</p>
+      <p>Salary details of Month</p>
+      <p>Total Salary :</p>
+      <button class="btn" style="width:100%"><i class="fa fa-download"></i> Download Report</button>
+    </div>
+	  </div>	
+  </div>
+
+
+  <!------------------------------Calender-------------------------------------->
+  <div class="right" style="background-color:#2F4F4F;">
+	  <div class="month">      
+		  <ul>
+			<li class="prev">&#10094;</li>
+			<li class="next">&#10095;</li>
+			<li>
+			  August<br>
+			  <span style="font-size:18px">2020</span>
+			</li>
+		  </ul>
+		</div>
+
+		<ul class="weekdays">
+		  <li>Mo</li>
+		  <li>Tu</li>
+		  <li>We</li>
+		  <li>Th</li>
+		  <li>Fr</li>
+		  <li>Sa</li>
+		  <li>Su</li>
+		</ul>
+
+		<ul class="days">  
+		  <li>1</li>
+		  <li>2</li>
+		  <li>3</li>
+		  <li>4</li>
+		  <li>5</li>
+		  <li>6</li>
+		  <li>7</li>
+		  <li>8</li>
+		  <li>9</li>
+		  <li><span class="active">10</span></li>
+		  <li>11</li>
+		  <li>12</li>
+		  <li>13</li>
+		  <li>14</li>
+		  <li>15</li>
+		  <li>16</li>
+		  <li>17</li>
+		  <li>18</li>
+		  <li>19</li>
+		  <li>20</li>
+		  <li>21</li>
+		  <li>22</li>
+		  <li>23</li>
+		  <li>24</li>
+		  <li>25</li>
+		  <li>26</li>
+		  <li>27</li>
+		  <li>28</li>
+		  <li>29</li>
+		  <li>30</li>
+		  <li>31</li>
+		</ul>
+	</div>
+</div>
+
+<!----------------------------------------Footer----------------------------->
+<div class="footer">
+  <p>Footer</p>
+</div>
+
+</body>
+</html>
