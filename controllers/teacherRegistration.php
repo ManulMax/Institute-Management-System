2f41dc20a1c4eacef981cc3bf7579700148ffa6c
ManulMax <manulmadara9725@gmail.com>
2020-11-18 13:05:20 +0530

    teacher registration with errs2

--- controllers/teacherRegistration.php
+++ controllers/teacherRegistration.php
@@ -13,6 +13,23 @@ class teacherRegistration extends Controller{
     }
 
     function create(){
+        
+        // echo $_POST['fname']." ";
+        // echo $_POST['mname']." ";
+        // echo $_POST['lname']." ";
+        // echo $_POST['NIC']." ";
+        // echo $_POST['DOB']." ";
+        // echo $_POST['gender']." ";
+        // echo $_POST['email']." ";
+        // echo $_POST['address']." ";
+        // echo $_POST['tel']." ";
+        // echo $_POST['qualifications']." ";        
+        // echo $_POST['id']." ";         
+        // echo $_POST['accNo']." ";
+        // echo $_POST['bank']." ";
+        // echo $_POST['today']." ";
+        // echo $_POST['branch']." ";
+
 
         $data = array();
         $data['fname'] = $_POST['fname'];
@@ -25,8 +42,7 @@ class teacherRegistration extends Controller{
         $data['address'] = $_POST['address'];
         $data['tel_no'] = $_POST['tel'];
         $data['qualifications'] = $_POST['qualifications'];        
-        $data['subject_id'] = $_POST['id'];
-                echo $subject_id;     
+        $data['subject_id'] = $_POST['id'];         
         $data['acc_no'] = $_POST['accNo'];
         $data['bank_name'] = $_POST['bank'];
         $data['reg_date'] = $_POST['today'];
