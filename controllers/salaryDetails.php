335d7b43d7c1912ab2d1ff052260680c880abf69
Isurika1998 <2018is055@stu.ucsc.cmb.ac.lk>
2021-01-30 21:37:48 +0530

    salary table

--- controllers/salaryDetails.php
+++ controllers/salaryDetails.php
@@ -10,6 +10,10 @@ class salaryDetails extends Controller{
     	$this->view->classList = $this->model->listClasses($_SESSION["userid"]);
     	$this->view->userDetails = $this->model->listDetails($_SESSION["userid"]);
     	$this->view->salary = $this->model->listSalaryDetails($_SESSION["userid"]);
+        $this->view->classSal1 = $this->model->listClassWissSalary($_SESSION["userid"],date("Y")." A/L");
+        $this->view->classSal2 = $this->model->listClassWissSalary($_SESSION["userid"],(date("Y")+1)." A/L");
+        $this->view->classSal3 = $this->model->listClassWissSalary($_SESSION["userid"],(date("Y")+2)." A/L");
+        $this->view->classSal4 = $this->model->listClassWissSalary($_SESSION["userid"],"Revision");
     	$this->view->render('teacher/salaryDetails');
     }
         
--- models/salaryDetails_model.php
+++ models/salaryDetails_model.php
@@ -24,68 +24,13 @@ class salaryDetails_Model extends Model{
 
     }
 
-    public function listHalls(){
 
-    	return $this->db->listAll("hall");
+    public function listClassWissSalary($userid,$batch){
 
-    }
-
-    public function listSubjects(){
-
-        return $this->db->listAll("subject");
-        
+        return $this->db->listWhere("SUM(f.amount)*95/100 as totalAmount","fees f,class c,teacher t","f.class_id=c.id and c.batch='".$batch."' and c.teacher_reg_no=t.reg_no and t.user_id=$userid");
+    
 
     }
-
-
-    public function getUser($id){
-
-        return $this->db->listWhere('user',array('nic','first_name','last_name','gender','email','contact_no','user_status','user_type'),"nic='$id'");
-    }
-
-    public function create($data){
-
-        $this->db->insert('user',array(
-            'nic' => $data['nic'],
-            'first_name' => $data['first_name'],
-            'last_name' => $data['last_name'],
-            'gender' => $data['gender'],
-            'email' => $data['email'],
-            'username' => $data['username'],
-            'password' => $data['password'],
-            'contact_no' => $data['contact_no'],
-            'user_status' => $data['user_status'],
-            'user_type' => $data['user_type']));
-
-
-    }
-
-    public function update($data){
-
-        $this->db->update('user',array(
-            'nic' => $data['nic'],
-            'first_name' => $data['first_name'],
-            'last_name' => $data['last_name'],
-            'gender' => $data['gender'],
-            'email' => $data['email'],
-            'username' => $data['email'],
-            'contact_no' => $data['contact_no'],
-            'user_status' => $data['user_status'],
-            'user_type' => $data['user_type']),"nic = '{$data['nic']}'");
-
-    }
-
-    public function delete($id){
-        $data = $this->db->listWhere('user',array('user_type'),"nic='$id'");
-
-        if($data['user_type']=='owner'){
-            return false;
-        } else{
-            $this->db->delete('user',"nic='$id'");
-        }
-
-    }
-
 
 
 
